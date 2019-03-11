2a64926a150881d36416f73a3361f8e0ae1bad6a
rigel <43711248+rigelselmani@users.noreply.github.com>
2019-03-10 23:31:59 -0400

    changes

--- assets/game.js
+++ assets/game.js
@@ -1,260 +1,91 @@
-$(document).ready(function() {
-
-    // ********** LOGIC FOR HOME PAGE **********
-    // Notifies the user of characters remaining in ownDefinition
-    var text_max = 140;
-    $("#textarea_feedback").html("Characters remaining: " + text_max);
-    $("#ownDefinition").keyup(function() {
-        var text_length = $("#ownDefinition").val().length;
-        var text_remaining = text_max - text_length;
-        $('#textarea_feedback').html("Characters remaining: " + text_remaining);
-    });
-    
-    // ********** LOGIC FOR SCAPE-STACK **********
-    var stackArray = [];
-    var stackWord;
-    var stackDef;
-    function popStackArray() {
-        stackArray = [];
-        for (i=0; i < localStorage.length; i++) {
-            var stackWord = localStorage.key(i);
-            var stackDef = localStorage.getItem(stackWord);
-            stackArray.push([stackWord, stackDef]);
-        }
-        console.log(stackArray);
-    
-        for (i=0; i < stackArray.length; i++) {
-            console.log(stackWord);
-            console.log(stackDef);
-            var stackWord = localStorage.key(i);
-            var stackDef = localStorage.getItem(stackWord);
-            var container = $("#stackGrid");
-            var emptyDiv = $("<div>");
-            var flashcards = $("<div>").addClass("uk-card uk-card-default uk-card-hover uk-card-body");
-            var h = $("<h3>").addClass("uk-card-title stackWord");
-            var p = $("<p>").addClass("stackDef");
-            h.html(stackWord);
-            h.append("<hr class=\"stackHr\">")
-            p.html(stackDef);
-            flashcards.append(h);
-            flashcards.append(p);
-            emptyDiv.append(flashcards);
-            container.append(emptyDiv);
-        }
-    }
-    popStackArray();
-    
-    // ********** LOGIC FOR PRONUNCIATION **********
-    var myArray = [];
-    
-    function displayPronunciation() {
-        // Grab the term from the input form
-        var word= $("#term-input").val().trim();
-        var key= "?key=1f40dde8-50d6-4eb6-9168-6f465c469eb9";
-        var queryUrl= "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+$(document).ready(function(){
+    var players = ["ronaldo","messi","hazard","neymar","bebeto"];
+     
+    function displayGif(){
+       
+        var player = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  player + "&api_key=dc6zaTOxFJmzC&limit=10";
         
-        // Clear out divs and arrays
-        $("#pronunciations").empty();
-        myArray = [];
-    
-        // Send query to Ajax
         $.ajax({
-            url: queryUrl + word + key,
-            method:"GET"
-        }).then(function(response) {
-            // Populate the array
-            // For each item in the response array
-            for (var i=0; i<response.length; i++) {
-                // Skip if the item doesn't have a prs parameter
-                if (response[i].hwi.prs == undefined) {
-                // If it has a prs parameter, then get the pos (fl) and prs
-                } else {
-                    var flValue = response[i].fl;
-                    var prsArray = response[i].hwi.prs;
-                    // For each item in the prs array
-                    for (var j=0; j<prsArray.length; j++) {
-                        // Skip if the prs doesn't have a sound parameter with it
-                        var wordArray = [];
-                        if (prsArray[j].sound == undefined) {
-                            // Skip here if undefined
-                        } else if ((myArray.toString()).includes(prsArray[j].sound.audio) == true) {
-                        } else {
-                            // If it has a sound parameter, grab the written pronunciation and audio file name, and along with flValue, send to myArray
-                            var writtenPron = prsArray[j].mw;
-                            var audioPron = prsArray[j].sound.audio;
-                            // Push the fl (pos), mw (written pronunciation) and audio (sound file name) into an array
-                            wordArray.push(flValue, writtenPron, audioPron);
-                            myArray.push(wordArray);
-                        }; // End of nested if statement
-                    }; // End of nested for statement
-                }; // End of first if statement
-            }; // End of for response.length statement
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
     
-            // Create Divs for each item in myArray
-            for (var i=0; i < myArray.length; i++) {
-                var pos = myArray[i][0];
-                var pron = myArray[i][1];
-                var file = myArray[i][2];
-                // Logic for subdirectory in order to create url
-                var uniChar = file.slice(0,1);
-                var biChar = file.slice(0,2);
-                var triChar = file.slice(0,3);
-                var subDir = "";
-                if (triChar == "bix") {
-                    subDir = "bix";
-                } else if (biChar == "gg") {
-                    subDir = "gg";
-                } else if (alphabetic(uniChar) == true) {
-                    subDir = uniChar;
-                } else {
-                    subDir = "number";
-                }
-                // Function to check letters
-                function alphabetic(inputtxt) {
-                    var letters = /^[a-zA-Z]+$/;
-                    if ((inputtxt.match(letters))) {
-                        return true;
-                    } else {
-                        return false; 
-                    }
-                }
-                var dDefault = $("<div>").addClass("uk-card").addClass("uk-card-default");
-                var dBody = $("<div>").addClass("uk-card-body");
-                var h = $("<h3>").addClass("uk-card-title");
-                var p = $("<p>").addClass("partofspeech");
-                var dBottom = $("<div>").addClass("uk-card-media-bottom");
-                var url = "https://media.merriam-webster.com/soundc11/" + subDir + "/" + file + ".wav";
-                h.text(pron);
-                p.text(pos);
-                dBody.append(h);
-                dBody.append(p);
-                dDefault.append(dBody);
-                dBottom.append("<audio id=\"embed_player\" src=" + url + " autostart=\"false\" controls=\"true\"></audio>");
-                dDefault.append(dBottom);
-                $("#pronunciations").append("<hr>");
-                $("#pronunciations").append(dDefault);
-            } // End of for myArray.length statement
-            $("#pronunciations").append("<br><br><br><br>"); 
-        }); // End of Ajax function
-    } // End of displayPronunciation function
+          var results = response.data;
+          for(var j =0;j<results.length;j++){
+            var displayDiv = $("<div>");
+            displayDiv.addClass("player-name");
+            var image = $("<img>");
+            image.attr("src", response.data[j].images.original_still.url);
+            image.attr("data-still", response.data[j].images.original_still.url);
+            image.attr("data-animate", response.data[j].images.original.url);
+            image.attr("data-state", "still");
+            image.attr("class", "gif");
+            displayDiv.append(image);
+            var rating = $('<p>').text('Rating: ' + results[j].rating.toUpperCase());
+        
+            var title = $('<p>').text('Title: ' + results[j].title.toUpperCase());
     
-    $(".search").on("click", function(event){
-        event.preventDefault();
-        var word = $("#term-input").val().trim();
-        displayPronunciation();
-    }); // End of search button click function
+            displayDiv.append(rating);
+            displayDiv.append(title);
+            displayDiv.append(image);
+            $("#player-view").prepend(displayDiv);
     
+          }
     
-    // ********** LOGIC FOR PRACTICE.HTML **********
-    var wordArray = [];
-    var wordLearned = 0;
-    var outerIndex = 0;
-    var wordIndex = 0;
-    var defIndex = 1;
-    var stackEnded = false;
+        
     
-    // Grab words and definitions from local storage and push to array
-    function popArray() {
-        wordArray = [];
-        for (i=0; i < localStorage.length; i++) {
-            var word = localStorage.key(i);
-            var def = localStorage.getItem(word);
-            wordArray.push([word, def]);
-        }
-    }
-    
-    function loadPractice() {
-        wordLearned = 0;
-        outerIndex = 0;
-        popArray();
-        stackEnded = false;
-    
-        function shuffleArray(array) {
-            for (let i = wordArray.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
-            }
-        }
-        shuffleArray();
-    
-        $("#wordDiv").text(wordArray[outerIndex][wordIndex]);
-        $("#wordDefDiv").text(wordArray[outerIndex][defIndex]).hide();
-        $("#wordCountDiv").text(wordArray.length);
-        $("#wordLearnedDiv").text(wordLearned)
-    
-    }; // End of loadPractice function
-    
-    // WHEN user clicks on the word
-    $(this).on("click", "#wordDiv", function() {
-        if (stackEnded == false) {
-            $("#wordDefDiv").show();
-            $("#wordDefDiv").addClass("uk-animation-scale-up");
-        }
     });
-    
-    // WHEN user clicks on Beginning button
-    $(this).on("click", "#btnBegin", function() {
-        if (wordArray.length == 0) {
-            endStack();
-        } else if (wordArray.length > 0) {
-            outerIndex = 0;
-            stackEnded = false;
-            $("#wordDiv").text(wordArray[outerIndex][wordIndex]);
-            $("#wordDefDiv").text(wordArray[outerIndex][defIndex]).hide();
-        }
-    });
-    
-    function endStack() {
-        var endOfStack = "— End of stack. —"
-        $("#wordDiv").html(endOfStack);
-        $("#wordDefDiv").hide();
-        stackEnded = true;
     };
+    function renderButtons(){
     
-    // WHEN user clicks on Next button
-    function nextWord() {
-        if (outerIndex > wordArray.length) {
-            endStack();
-        } else if (outerIndex == wordArray.length) {
-            endStack();
-        } else if (outerIndex < wordArray.length) {
-            outerIndex++;
-            if (outerIndex == wordArray.length) {
-                endStack();
-            } else if (outerIndex < wordArray.length) {
-                $("#wordDiv").text(wordArray[outerIndex][wordIndex]);
-                $("#wordDefDiv").text(wordArray[outerIndex][defIndex]).hide();
-            }
+        $(".display-buttons").empty();
+    
+        for (var i = 0; i < players.length; i++){
+    
+            var a = $("<button>");
+            a.addClass("topic");
+            // Adding a class for styling
+            a.addClass("btn btn-lg");
+            // Adding a data-attribute
+            a.attr("data-name", players[i]);
+            // adding attr for Bootstrap
+            a.attr("type", "button");
+            // Button Text
+            a.text(players[i]);
+            // Adding the Button to the Buttons Div
+            $(".display-buttons").append(a);
         }
     }
+    function playGif() {
     
-    $(this).on("click", "#btnNext", function() {
-        if (outerIndex <= wordArray.length - 1) {
-            nextWord();
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+    
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
         }
-    });
     
-    // WHEN user clicks on GotIt button
-    $(this).on("click", "#btnGotIt", function() {
-        if (wordArray.length == 0) {
-            endStack();
-        } else if (stackEnded == true) {
-            // Do nothing 
-        } else if (wordArray.length > 0) {
-            wordArray.splice(outerIndex, 1);
-            $("#wordCountDiv").text(wordArray.length);
-            wordLearned++;
-            $("#wordLearnedDiv").text(wordLearned);
-            outerIndex--;
-            nextWord();
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
         }
+    }
+    $("#subPress").on("click",function(event){
+        event.preventDefault();
+        var inputPlayer =$("#player-input").val().trim();
+        players.push(inputPlayer);
+        $("form").trigger("reset")
+        renderButtons();
+        return false;
+        
+    
     });
+    renderButtons();
     
-    // WHEN user clicks on StartOver button
-    $(this).on("click", "#btnStartOver", function() {
-        loadPractice();
+    $(document).on("click", ".topic",displayGif)
+    // Click on the Gif image to animate or make it still
+    $(document).on("click", ".gif", playGif)
     });
-    
-    loadPractice();
-    
-    }); // End of $(document).ready function
